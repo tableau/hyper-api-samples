5b41a58ccc14e4ac804479100e3908e49aed4a21
tsjoblad <tsjoblad@tableau.com>
2020-06-25 08:42:25 -0700

    hyper release v0.11074

--- Tableau-Supported/DotNet/build.sh
+++ Tableau-Supported/DotNet/build.sh
@@ -1,4 +1,4 @@
 set -e
 dotnet build Example.csproj
-cp -R ../lib/hyper bin/Debug/netcoreapp2.2
-cp -R ../lib/libtableauhyperapi.* bin/Debug/netcoreapp2.2/
+cp -R ../lib/hyper bin/Debug/netcoreapp3.1
+cp -R ../lib/libtableauhyperapi.* bin/Debug/netcoreapp3.1/
