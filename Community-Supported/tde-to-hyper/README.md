7ff43088c5db678dec9b9e1cb2a8be1ad952100d
Adrian Vogelsgesang <avogelsgesang@salesforce.com>
2023-08-29 10:22:38 -0700

    Improve TDE -> Hyper upgrade script (#98)
    * Schema and table names are now correctly handled
    * Upgrading files with non-standard schemas / tables is now supported
    * The script can now also work on complete folders

--- Community-Supported/tde-to-hyper/README.md
+++ Community-Supported/tde-to-hyper/README.md
@@ -1,54 +1,54 @@
 # tde-to-hyper
-## __tde_to_hyper__
 
 ![Community Supported](https://img.shields.io/badge/Support%20Level-Community%20Supported-53bd92.svg)
 
-__Current Version__: 1.0
-
-This script can be used to create a `.hyper` file from a `.tde` file.
+The `.tde` file format will be deprecated by end of 2023 (see [deprecation announcement](https://community.tableau.com/s/feed/0D54T00001BHiGwSAL)).
+This script upgrades `.tde` files to `.hyper` files.
 
 # Get started
 
-## __Prerequisites__
+## Prerequisites
 
 To run the script, you will need:
 
-- a computer running Windows, macOS, or Linux
-
+- A computer running Windows, macOS, or Linux
 - Python (3.7 or newer)
-
-- Install the Hyper API `pip install -r requirements.txt`
+- Install the Hyper API version `0.0.17537` (`pip install -r requirements.txt`)
 
 ## Run the sample
 
-Ensure that you have installed the requirements and then just run the sample Python file.
-The following instructions assume that you have set up a virtual environment for Python. For more information on
-creating virtual environments, see [venv - Creation of virtual environments](https://docs.python.org/3/library/venv.html)
-in the Python Standard Library.
+> **_NOTE:_**  The following command lines are for Linux or macOS and need to be slightly adapted for Windows.
 
-1. Open a terminal and activate the Python virtual environment (`venv`).
+The following instructions assume that you want to use a virtual environment for Python. For more information on
+creating virtual environments, see [venv - Creation of virtual environments](https://docs.python.org/3/library/venv.html).
 
-1. Navigate to the folder where you installed the sample.
-
+1. Open a terminal and navigate to the folder of the `tde_to_hyper.py` file
+1. Create a virtual environment and install Python Hyper API
+  ```
+  $ python3 -m venv .venv/
+  $ .venv/bin/python3 -m pip install -r requirements.txt
+  ```
 1. Run the Python script:
-   
-   **python tde_to_hyper.py input_tde_path**
-
    The script requires a path to a tde file and will convert the tde to a `.hyper` file. The `.hyper` file will be created in the directory of the tde file.
-
    Example:
-
    ```cli
-   (venv)c:\mydir> python tde_to_hyper.py extract.tde
+   $ .venv/bin/python3 tde_to_hyper.py extract.tde
    Successfully converted extract.tde to extract.hyper
-
-   (venv)c:\mydir> python tde_to_hyper.py c:\path\to\file\extract.tde
-   Successfully converted c:\path\to\file\extract.tde to c:\path\to\file\extract.hyper
+   $ .venv/bin/python3 tde_to_hyper.py path/to/file/extract.tde
+   Successfully converted path/to/file/extract.tde to path/to/file/extract.hyper
    ```
 
-## __Resources__
-Check out these resources to learn more:
+## Advanced usage
+
+The script can also be invoked on a folder and will upgrade each `.tde` file within
+that folder. The upgraded `.hyper` files will be placed directly next to the `.tde`
+files. If the new `.hyper` files should be written to a differnt folder, the
+`--output` can be used to specify a location where the upgraded `.hyper` should be
+written.
+
+## Resources
 
 - [Hyper API docs](https://tableau.github.io/hyper-db)
-
 - [Tableau Hyper API Reference (Python)](https://tableau.github.io/hyper-db/lang_docs/py/index.html)
+- [TDE deprecation announcement](https://community.tableau.com/s/feed/0D54T00001BHiGwSAL)
+- [August 2023 update on TDE deprecation](https://community.tableau.com/s/feed/0D58b0000BTEIShCQP)
--- Community-Supported/tde-to-hyper/tde_to_hyper.py
+++ Community-Supported/tde-to-hyper/tde_to_hyper.py
@@ -1,51 +1,47 @@
 import argparse
+import os
 from pathlib import Path
-from tableauhyperapi import HyperProcess, Telemetry, \
-    Connection, CreateMode, TableDefinition, escape_name, \
-    escape_string_literal
+from tableauhyperapi import HyperProcess, Telemetry, Connection, CreateMode, escape_string_literal, TableName, HyperException
 
-def convert_tde_to_hyper(tde_path: Path):
-    # Rename path with a .hyper extension in the directory of the tde file
-    hyper_database = tde_path.with_name(tde_path.stem + '.hyper')
-
-    with HyperProcess(telemetry=Telemetry.SEND_USAGE_DATA_TO_TABLEAU) as hyper:
-        with Connection(endpoint=hyper.endpoint, database=hyper_database, create_mode=CreateMode.CREATE_AND_REPLACE) as connection:
-            # Schema and table for TDE file is constant
-            schema = 'Extract'
-            table = 'Extract'
-
-            # Create the temp external table for the TDE file
-            create_external_table_query = _get_external_table_query(str(tde_path), schema, table)
-            connection.execute_command(create_external_table_query)
-
-            # Get the name of the table created from the catalog
-            td = connection.catalog.get_table_definition(table)
-
-            # Create the schema
-            connection.catalog.create_schema(schema)
-
-            # Create the destination table in the Hyper database
-            schema_table = f"\"{schema}\".\"{table}\"" 
-            create_table_command = f"CREATE TABLE {escape_name(schema_table)} AS SELECT * FROM {td.table_name}"
-
-            # Execute
-            connection.execute_command(create_table_command)
-
-    print(f"Successfully converted {tde_path} to {hyper_database}")
-
-def _get_external_table_query(tde_file_path: str,
-                              schema: str,
-                              table: str):
-    return f"""CREATE TEMP EXTERNAL TABLE {escape_name(table)} FOR {escape_string_literal(tde_file_path)} 
-    (WITH (FORMAT TDE, TABLE {escape_string_literal(f"{schema}.{table}")}, SANITIZE))"""
+def convert_tde_to_hyper(hyper_endpoint: str, tde_path: Path, hyper_path: Path):
+    os.makedirs(hyper_path.parent.absolute(), exist_ok = True)
+    with Connection(endpoint=hyper_endpoint, database=hyper_database, create_mode=CreateMode.CREATE_AND_REPLACE) as connection:
+        try:
+            existing_tables = connection.execute_list_query(f"""
+                SELECT "SCHEMAS_NAME", "TABLES_NAME"
+                FROM external({escape_string_literal(str(tde_path))}, format => 'tde', "table" => 'SYS.TABLES')
+                JOIN external({escape_string_literal(str(tde_path))}, format => 'tde', "table" => 'SYS.SCHEMAS')
+                ON "TABLES_PARENT"="SCHEMAS_ID"
+                WHERE "SCHEMAS_NAME" <> 'SYS' AND "TABLES_NAME"<>'$TableauMetadata' AND "TABLES_ACTIVE" AND "SCHEMAS_ACTIVE"
+            """)
+            for schema, table in existing_tables:
+                # Create the destination table in the Hyper database
+                connection.catalog.create_schema_if_not_exists(schema)
+                connection.execute_command(f"""
+                    CREATE TABLE {TableName(schema, table)} AS
+                    SELECT * FROM external({escape_string_literal(str(tde_path))}, format => 'tde', "table" => {escape_string_literal(f"{schema}.{table}")})""")
+        except HyperException:
+            os.unlink(hyper_database)
+            print(f"FAILED conversion {tde_path} -> {hyper_database}")
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser(description="Script to convert a TDE file to a Hyper file.")
     argparser.add_argument("input_tde_path", type=Path, help="The input TDE file path that will be converted to a Hyper file.")
+    argparser.add_argument("--output", type=Path, help="The output path.", default=".")
     args = argparser.parse_args()
     
     input_tde_path = Path(args.input_tde_path)
     if not input_tde_path.exists():
         raise Exception(f"{input_tde_path} not found")
 
-    convert_tde_to_hyper(input_tde_path)
+    if input_tde_path.is_dir():
+        inputs = list(input_tde_path.glob("**/*.tde"))
+    else:
+        inputs = [input_tde_path]
+
+    with HyperProcess(telemetry=Telemetry.SEND_USAGE_DATA_TO_TABLEAU) as hyper:
+        for tde_path in inputs:
+            # Rename path with a .hyper extension in the directory of the tde file
+            hyper_database = args.output / tde_path.with_name(tde_path.stem + '.hyper')
+            convert_tde_to_hyper(hyper.endpoint, tde_path, hyper_database)
+
